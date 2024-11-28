0f4fd2b553c4a39126a33ac8b6a28b2ecf8f9fd6
Thércio Rodrigues <thercior@gmail.com>
2024-11-28 14:19:05 -0300

    :recycle: refactor: adicionado parâmetro de statics na url

--- config/urls.py
+++ config/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from apps.webhook_notify.views import WebhookNotify
@@ -7,3 +9,6 @@ urlpatterns = [
     path('api/v1/', include('apps.webhook_notify.urls', namespace='Notify')),
     path('api/v1/', include('apps.webhook_password.urls', namespace='Password')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
